5befa339c2ec06163347d06bf8a9a82a69183e54
Jeff <jeffrey.bursey8806@live.com>
2021-01-22 11:31:00 -0600

    Unbreak game. Reverting back to keeping canvas state contained.

---
+++ src/game2.tsx
@@ -0,0 +1,233 @@
+import * as React from "react";
+import {useRef, useEffect} from "react"
+import {GameState, KeyCode, Point} from "./models";
+
+const Game2 = (props : any) => {
+    let state : GameState;
+    let canvas: HTMLCanvasElement;
+    let context: CanvasRenderingContext2D;
+    let canvasRef = useRef<HTMLCanvasElement>(null);
+    let reqAnimationFrame : number;
+    let then: Date;
+    let msSinceUpdate : number;
+
+    const onKeyboardEvent  = (event : KeyboardEvent) => {
+        let keyCode : KeyCode = KeyCode.NONE;
+        switch(event.key)
+        {
+            case "ArrowUp":
+                //setGameState({...gameState, lastKnownKeyCode: KeyCode.UP});
+                keyCode = KeyCode.UP;
+                break;
+            case "ArrowDown":
+                //setGameState({...gameState, lastKnownKeyCode: KeyCode.DOWN});
+                keyCode = KeyCode.DOWN;
+                break;
+            case "ArrowLeft":
+                //setGameState({...gameState, lastKnownKeyCode: KeyCode.LEFT});
+                keyCode = KeyCode.LEFT;
+                break;
+            case "ArrowRight":
+                //setGameState({...gameState, lastKnownKeyCode: KeyCode.RIGHT});
+                keyCode = KeyCode.RIGHT;
+                break;
+            case "Escape":
+            default:
+                //setGameState({...gameState, lastKnownKeyCode: KeyCode.ESC});
+                keyCode = KeyCode.ESC;
+                break;
+        }
+
+        state.lastKnownKeyCode = keyCode;
+        //console.log("Key: ", keyCode.toString());
+    }
+
+    const clear = (context: CanvasRenderingContext2D) => {
+        //console.log("RenderTarget clear");
+        context.clearRect(0, 0, state.width, state.height);
+        context.fillStyle = "#000";
+        context.fillRect(0, 0, state.width, state.height);
+    }
+
+    const draw = (gameState: GameState, context: CanvasRenderingContext2D) => {
+        //console.log("RenderTarget draw");
+
+        //food
+        context.fillStyle = "#FF0000";
+        context.fillRect(gameState.foodPosition.x, gameState.foodPosition.y, gameState.cellSize, gameState.cellSize);
+
+        //snake
+        let first: boolean = false;
+        gameState.snakePosition.forEach((part: Point) => {
+            context.fillStyle = "#00FF00";
+            if(!first)
+            {
+                first = true;
+                //context.fillStyle = "#0000FF";
+                context.strokeStyle = "#0000FF";
+                context.lineWidth = 5;
+                context.strokeRect(part.x, part.y, gameState.cellSize, gameState.cellSize);
+            }
+            
+            context.fillRect(part.x, part.y, gameState.cellSize, gameState.cellSize);
+        });
+    }
+
+    const moveSnake = () => {
+        let pos : Point = {
+            x: state.snakePosition[0].x,
+            y: state.snakePosition[0].y
+        }
+
+        switch(state.lastKnownKeyCode)
+        {
+            case KeyCode.UP:
+                pos.y -= state.cellSize;
+                break;
+            case KeyCode.DOWN:
+                pos.y += state.cellSize;
+                break;
+            case KeyCode.LEFT:
+                pos.x -= state.cellSize;
+                break;
+            case KeyCode.RIGHT:
+                pos.x += state.cellSize;
+                break;
+            case KeyCode.NONE:
+            case KeyCode.ESC:
+                return; //do nothing
+        }
+        
+        //new head
+        let newSnakePosition: Point[] = [];
+        newSnakePosition.push(pos);
+        for(let i = 1; i < state.snakePosition.length; i++)
+        {
+            newSnakePosition.push({x: state.snakePosition[i-1].x, y: state.snakePosition[i-1].y});
+        }
+        state.snakePosition = newSnakePosition;
+        //console.log("Moving snake: ", state.snakePosition[0], state.lastKnownKeyCode);
+    };
+
+    const getRandomCellPosition = () : Point => {
+        let x: number = 0;
+        let y: number = 0;
+
+        x = Math.floor((Math.random() * 10000) % (state.width - state.cellSize));
+        y = Math.floor((Math.random() * 10000) % (state.height - state.cellSize));
+
+        x = x - (x % state.cellSize);
+        y = y - (y % state.cellSize);
+
+        return {
+            x: x,
+            y: y
+        };
+    };
+
+    const tryEatFood = () => {
+        let head = state.snakePosition[0];
+        let food = state.foodPosition;
+
+        if(head.x == food.x && head.y == food.y)
+        {
+            //move it grow the snake
+            let newSnakePos: Point[] = [];
+            
+            //push "food" to snake at its end opposite of the direction
+            let tailPos = state.snakePosition[state.snakePosition.length - 1];
+            let newTailPos : Point = {x: tailPos.x, y: tailPos.y};
+            switch(state.lastKnownKeyCode)
+            {
+                case KeyCode.UP:
+                    newTailPos.y += state.cellSize;
+                    break;
+                case KeyCode.DOWN:
+                    newTailPos.y -= state.cellSize;
+                    break;
+                case KeyCode.LEFT:
+                    newTailPos.x += state.cellSize;
+                    break;
+                case KeyCode.RIGHT:
+                    newTailPos.x -= state.cellSize;
+                    break;
+                case KeyCode.NONE:
+                case KeyCode.ESC:
+                    return; //do nothing
+            }
+        
+            for(let i = 0; i < state.snakePosition.length; i++)
+            {
+                newSnakePos.push({x: state.snakePosition[i].x, y: state.snakePosition[i].y});
+            }
+            newSnakePos.push(newTailPos);
+
+            state.snakePosition = newSnakePos;
+            state.foodPosition = getRandomCellPosition();
+        }
+    };
+
+    const render = () => {
+        let now : Date = new Date();
+
+        let dt = now.getTime() - then.getTime();
+        msSinceUpdate += dt;
+
+        then = now;
+
+        clear(context);
+        //console.log(msSinceUpdate, state.updateDelayMS);
+        if(msSinceUpdate > state.updateDelayMS) //make part of prop initial
+        {
+            //move in last known direction
+            moveSnake();
+            //check for food eaten
+            tryEatFood();
+
+            msSinceUpdate = 0;
+        }
+
+        draw(state, context);
+
+        reqAnimationFrame = window.requestAnimationFrame(render);
+    };
+
+    useEffect(() => {
+        then = new Date();
+        msSinceUpdate = 0;
+        state = {
+            cellSize: 20,
+            height: window.innerHeight,
+            width: window.innerWidth,
+            lastKnownKeyCode: KeyCode.NONE,
+            updateDelayMS: 200,
+            foodPosition: {x: 0, y: 0},
+            snakePosition: [{
+                x: 50,
+                y: 50
+            }]
+        };
+        state.snakePosition[0] = getRandomCellPosition();
+        state.foodPosition = getRandomCellPosition();
+
+        canvas = canvasRef.current;
+        canvas.width = state.width;
+        canvas.height = state.height;
+
+        context = canvas.getContext("2d");
+
+        window.onkeyup = onKeyboardEvent;
+        render();
+
+        return () => {
+            console.log("Cancelling animation frame");
+            window.cancelAnimationFrame(reqAnimationFrame);
+        }
+    }, []);
+
+    return (
+        <canvas ref={canvasRef}></canvas>
+    )
+}
+
+export default Game2;
--- src/index.tsx
+++ src/index.tsx
@@ -1,107 +1,14 @@
 import * as React from "react"
 import {useState, useEffect} from "react"
 import * as ReactDom from "react-dom"
-import Game from "./game"
-import Menu from "./menu"
-import RenderTarget from "./renderTarget"
-import { GameState, KeyCode, Point } from "./models"
+import Game from "./game2"
 
-const initialState: GameState = {
-    cellSize: 20,
-    height: window.innerHeight,
-    width: window.innerWidth,
-    lastKnownKeyCode: KeyCode.NONE,
-    updateDelayMS: 500,
-    foodPosition: {
-        x: 20,
-        y: 20
-    },
-    snakePosition: [{x: 100, y: 100}]
-}
 
 const App = (props: any) => {
-    const [gameState, setGameState] = useState<GameState>(initialState);
-
-    const onKeyPress = (event : KeyboardEvent) => {
-        let keyCode : KeyCode = KeyCode.NONE;
-        switch(event.key)
-        {
-            case "ArrowUp":
-                //setGameState({...gameState, lastKnownKeyCode: KeyCode.UP});
-                keyCode = KeyCode.UP;
-                break;
-            case "ArrowDown":
-                //setGameState({...gameState, lastKnownKeyCode: KeyCode.DOWN});
-                keyCode = KeyCode.DOWN;
-                break;
-            case "ArrowLeft":
-                //setGameState({...gameState, lastKnownKeyCode: KeyCode.LEFT});
-                keyCode = KeyCode.LEFT;
-                break;
-            case "ArrowRight":
-                //setGameState({...gameState, lastKnownKeyCode: KeyCode.RIGHT});
-                keyCode = KeyCode.RIGHT;
-                break;
-            case "Escape":
-            default:
-                //setGameState({...gameState, lastKnownKeyCode: KeyCode.ESC});
-                keyCode = KeyCode.ESC;
-                break;
-        }
-
-        //--move snake
-        moveSnake(keyCode)
-    }
-
-    const moveSnake = (keyCode : KeyCode) => {
-        let pos : Point = {
-            x: gameState.snakePosition[0].x,
-            y: gameState.snakePosition[0].y
-        }
-
-        switch(keyCode)
-        {
-            case KeyCode.UP:
-                pos.y -= gameState.cellSize;
-                break;
-            case KeyCode.DOWN:
-                pos.y += gameState.cellSize;
-                break;
-            case KeyCode.LEFT:
-                pos.x -= gameState.cellSize;
-                break;
-            case KeyCode.RIGHT:
-                pos.x += gameState.cellSize;
-                break;
-        }
-        console.log("Moving snake: ", pos, keyCode);
-        setGameState({...gameState, snakePosition: [pos], lastKnownKeyCode: keyCode});
-    };
-
-    useEffect(() => {
-        window.onkeyup = onKeyPress;
-    }, []);
-
-    const onMenuItemSelected = (name: string) => {
-        
-        switch(name)
-        {
-            case "Start":
-                //start/resume game and hide menu
-                break;
-            case "High Scores":
-                //pause game if started show high scores
-                break;
-            case "Options":
-                //pause game if started show options
-                break;
-        }
-    }
-
+    
     return (
         <>
-        <RenderTarget {...gameState} />
-        <Menu onMenuItemSelected={onMenuItemSelected} />
+            <Game />
         </>        
     )    
 }
